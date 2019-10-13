c0f0c66aabf72da6f433cda97670b980893ea470
Jean-Pierre Thach <jeanpierrethach@gmail.com>
2019-10-13 12:05:25 -0400

    Fixed dataset folder reference

--- src/predictions.py
+++ src/predictions.py
@@ -6,8 +6,8 @@ import csv
 from vectorizer import CountVectorizer, TfidfVectorizer
 from multinb import MultinomialNaiveBayes
 
-data = np.load("../data_train.pkl", allow_pickle=True)
-test_data = np.load("../data_test.pkl", allow_pickle=True)
+data = np.load("../data/data_train.pkl", allow_pickle=True)
+test_data = np.load("../data/data_test.pkl", allow_pickle=True)
 
 train_set = data[0]
 train_labels = data[1]
--- src/validation.py
+++ src/validation.py
@@ -5,8 +5,8 @@ from preprocess import preprocess
 from vectorizer import CountVectorizer, TfidfVectorizer
 from multinb import MultinomialNaiveBayes
 
-data = np.load("../data_train.pkl", allow_pickle=True)
-test_data = np.load("../data_test.pkl", allow_pickle=True)
+data = np.load("../data/data_train.pkl", allow_pickle=True)
+test_data = np.load("../data/data_test.pkl", allow_pickle=True)
 
 train_set, train_labels, valid_set, valid_labels = split_dataset(data, N=49000, random_seed=3395)
 
